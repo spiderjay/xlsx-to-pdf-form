ff4e166d2b7193a0287f399d48ca7fe5fc858cc3
Jay Phillips <jay@spiderjay.com>
2020-01-29 16:24:10 -0500

    Revert "Handling for StateOptions (checkboxes)"
    This reverts commit 79b22a0990431723dac85023e3825cfe441bb799.

--- webtool.php
+++ webtool.php
@@ -1,16 +1,10 @@
 <?php
 
-require_once("./map.php");
-require_once("./vals.php");
-
-
 $xfdf_head = '<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><fields>';
 $xml_data = '';
 $xfdf_end = '</fields></xfdf>';
 
 /* Generate all fields with field_key form webform and value form submission */
-
-/*
 
 $fields = array(
 	"topmostSubform[0].Page1[0].p1-t1[0]" => "My name goes here"
@@ -23,40 +17,6 @@ foreach ($fields as $key => $value) {
             <value>'.$value.'</value>
         </field>';
 }
-*/
-
-foreach ($values as $key => $val) {
-
-	echo "Mapping field name [". $mapped_fields[$key]['Name'] ." to value [$val]\n";
-
-	// if we have an array of options for the value this is a checkbox
-	if (isset($mapped_fields[$key]["StateOption"])) {
-		//print_r($mapped_fields[$key]);
-
-		//echo "Field has state options\n";
-
-		if ($val != "Off") {
-
-			//echo "-- Field should NOT be off\n";
-
-			$search = array_search("Off", $mapped_fields[$key]["StateOption"]);
-			$val = $search == 0 ? $mapped_fields[$key]["StateOption"][1] : $mapped_fields[$key]["StateOption"][0];
-
-			//echo "-- Value set to [$val]\n";
-		}
-	}
-
-	$state = "";
-	if (isset($mapped_fields[$key]["StateOption"])){
-		$state = '<stateoption>'.$val.'</stateoption>';
-	}
-
-    $xml_data .= '
-        <field name="'.$mapped_fields[$key]['Name'].'">
-            <value>'.$val.'</value>
-        </field>';
-}
-
 
 
 $FDF_content = $xfdf_head.$xml_data.$xfdf_end;
