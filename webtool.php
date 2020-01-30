5b074d8f088fc989c698970236098579da3966b8
Jay Phillips <jay@spiderjay.com>
2020-01-30 12:20:12 -0500

    Added class structure and function for verifying changes to forms

--- webtool.php
+++ webtool.php
@@ -7,8 +7,89 @@
 
 */
 
+define("PATH_ROOT",	getcwd());
+define("PATH_FORMS", PATH_ROOT . "/forms/");
+
+
+class excelToPdfForm {
+
+	// Check modified date of all forms
+	function formsForReprocessing() {
+
+		$dates = array();
+		$dates_file = PATH_FORMS . "dates.txt";
+		$forms_to_reprocess = array();
+
+
+		// Load last modified dates (if available)
+		if (is_file($dates_file)) {
+			
+			$handle = fopen($dates_file, "r");
+			
+			if ($handle) {
+			    while (($line = fgets($handle)) !== false) {
+			    	if (trim($line) != "") {
+						$exp = explode(" ", $line);
+						$dates[$exp[0]] = trim($exp[1]);
+					}
+				}
+			}
+		}
+
+		// Find all the files in the pdf form directory
+		$dir = PATH_FORMS;
+		$files = array_diff(scandir($dir), array('.', '..'));
+
+		$str = "";
+
+		foreach ($files as $file) {
+			
+			$file_path = $dir . $file;
+
+			// Get modified date only for PDF files
+			if (is_file($file_path) && mime_content_type($file_path) == "application/pdf") {
+				$mod_date = filemtime($file_path);
+				$str .= "$file $mod_date\n";
+
+				if (isset($dates[$file]) && $mod_date != $dates[$file]) {
+					echo "Date mismatch for $file... dates file says [{$dates[$file]}] but file is [$mod_date]\n";
+					$forms_to_reprocess[] = $file;
+				}
+			}
+		}
+
+		// Write new modified dates to file
+		//file_put_contents($dates_file, $str);
+
+		if (count($forms_to_reprocess)) {
+			return $forms_to_reprocess;
+		}
+		
+		return false;
+
+	}
+
+}
+
+$x = new excelToPdfForm();
+
+if ($forms_to_reprocess = $x->formsForReprocessing()) {
+	echo "We must reprocess the following forms:\n";
+	print_r($forms_to_reprocess);
+}
+
+die;
+
+
+
 require_once("./map.php");
 require_once("./vals.php");
+
+
+// Verify that the original form(s) have not been altered
+
+
+
 
 
 $xfdf_head = '<?xml version="1.0" encoding="UTF-8"?><xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve"><fields>';
